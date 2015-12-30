0ac950eb84b2821d375c860974783ec0c2b8c95e
pon <pungpon_sundy@hotmail.com>
2015-12-30 13:08:14 +0700

    9  record

--- app/src/main/java/apprtc/pn/carbuddythailand/MainActivity.java
+++ app/src/main/java/apprtc/pn/carbuddythailand/MainActivity.java
@@ -1,5 +1,6 @@
 package apprtc.pn.carbuddythailand;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,6 +16,33 @@ public class MainActivity extends AppCompatActivity {
         //Setup Constructor
         objManageTABLE = new ManaggeTABLE(this);
 
+        //Test Add Value
+        //objManageTABLE.addNewValue("testCat", "testName","testPhone" );
+
+        //Delete All Data
+        deleteAllData();
+
+        //Add Data to my SQLite
+        addDataToSQLite();
+
     }   //Main Method
+
+    private void addDataToSQLite() {
+
+        //Get data
+        String[] categoryStrings = MyData.CategorySTRINGS;
+        String[] nameStrings = MyData.nameSTRINGS;
+        String[] phoneStrings = MyData.phonSTRINGS;
+
+        for (int i=0;i<categoryStrings.length;i++){
+            objManageTABLE.addNewValue(categoryStrings[i], nameStrings[i], phoneStrings[i]);
+        } //for
+
+    }   //addDataToSQLite
+
+    private void deleteAllData() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME, MODE_PRIVATE, null);
+        objSqLiteDatabase.delete(ManaggeTABLE.TABLE_PHONE, null,null);
+    }
 
 } //Main Call
--- app/src/main/java/apprtc/pn/carbuddythailand/ManaggeTABLE.java
+++ app/src/main/java/apprtc/pn/carbuddythailand/ManaggeTABLE.java
@@ -1,5 +1,6 @@
 package apprtc.pn.carbuddythailand;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -11,6 +12,12 @@ public class ManaggeTABLE {
     private  MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String TABLE_PHONE ="phoneTABLE";
+    public static final String COLUMN_ID ="_id";
+    public static final String COLUMN_CATEGORY = "Category";
+    public static final String COLUMN_NAME ="Name";
+    public static final String COLUMN_PHONE ="Phone";
+
     public ManaggeTABLE(Context context) {
 
         //Create & Connected Database
@@ -19,5 +26,16 @@ public class ManaggeTABLE {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }   //Constructor
+
+    //Add New Value to SQLite
+    public Long addNewValue(String StrCategory, String StrName, String strPhone) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_CATEGORY, StrCategory);
+        objContentValues.put(COLUMN_NAME, StrName);
+        objContentValues.put(COLUMN_PHONE, strPhone);
+        return writeSqLiteDatabase.insert(TABLE_PHONE, null,objContentValues);
+    }
+
 
 }   //Main Clss
